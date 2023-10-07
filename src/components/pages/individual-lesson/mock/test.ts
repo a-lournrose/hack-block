9f7b5c55a846b42e75b3a35b3ad833b2c24b6e60
DenisRybkin <denis.rybkin.94@mail.ru>
2023-10-07 04:05:55 +0300

    add default question

--- src/components/pages/individual-lesson/mock/test.ts
+++ src/components/pages/individual-lesson/mock/test.ts
@@ -36,25 +36,127 @@ const questions: (QuestionDto & { topTitle?: string })[] = [
   {
     id: 2,
     testId: 0,
-    title: '',
+    imageSrc:
+      'https://sun9-66.userapi.com/impg/HbUEbhaM3eeOUl_VVL1K6ZkDJoAIl8aQokKCvw/ikXMIgpf-qw.jpg?size=650x500&quality=96&sign=075fd003ab9aa904b99b4ade2f696666&type=album',
+    title:
+      'Определите по карте расстояние на местности по прямой от родника до дома лесника. Измерение проводите между центрами условных знаков. Полученный результат округлите до десятков метров. Выберите вариант ответа.',
     answers: [
       {
         id: 1,
         questionId: 0,
         isRight: false,
-        title: '',
+        title: '310',
       },
+      {
+        id: 2,
+        questionId: 0,
+        isRight: true,
+        title: '200',
+      },
+      {
+        id: 3,
+        questionId: 0,
+        isRight: false,
+        title: '150',
+      },
+      {
+        id: 4,
+        questionId: 0,
+        isRight: false,
+        title: '320',
+      },
+    ],
+  },
+  {
+    id: 3,
+    testId: 0,
+    title: 'Как называется наиболее крупный океан на Земле?',
+    answers: [
       {
         id: 1,
         questionId: 0,
         isRight: false,
-        title: '',
+        title: 'Атлантический океан.',
       },
+      {
+        id: 2,
+        questionId: 0,
+        isRight: false,
+        title: 'Индийский океан.',
+      },
+      {
+        id: 3,
+        questionId: 0,
+        isRight: false,
+        title: 'Северно-ледовитый океан.',
+      },
+      {
+        id: 4,
+        questionId: 0,
+        isRight: true,
+        title: 'Тихий океан.',
+      },
+    ],
+  },
+  {
+    id: 4,
+    testId: 0,
+    title: 'Какое государство является самым крупным по площади в мире?',
+    answers: [
       {
         id: 1,
         questionId: 0,
         isRight: false,
-        title: '',
+        title: 'США',
+      },
+      {
+        id: 2,
+        questionId: 0,
+        isRight: false,
+        title: 'Германия',
+      },
+      {
+        id: 3,
+        questionId: 0,
+        isRight: false,
+        title: 'Канада',
+      },
+      {
+        id: 4,
+        questionId: 0,
+        isRight: true,
+        title: 'Российская Федерация.',
+      },
+    ],
+  },
+  {
+    id: 5,
+    testId: 0,
+    title: 'Какие государства входят в Южную Америку?',
+    answers: [
+      {
+        id: 1,
+        questionId: 0,
+        isRight: false,
+        title: 'Аргентина, Боливия, Бразилия',
+      },
+      {
+        id: 2,
+        questionId: 0,
+        isRight: false,
+        title: 'Германия, Швеция, Швейцария',
+      },
+      {
+        id: 3,
+        questionId: 0,
+        isRight: false,
+        title: 'Австралия, Япония и Индия',
+      },
+      {
+        id: 4,
+        questionId: 0,
+        isRight: true,
+        title: 'США, Канада, Мексика',
       },
     ],
   },
