27f37728510864cb99f88d3442751795350f62ef
DenisRybkin <denis.rybkin.94@mail.ru>
2023-10-07 06:58:34 +0300

    finish

--- src/components/pages/individual-lesson/lesson-test.tsx
+++ src/components/pages/individual-lesson/lesson-test.tsx
@@ -10,23 +10,26 @@ interface ILessonTest {
   questions: QuestionDto[];
 }
 
-export const LessonTest:FC<ILessonTest> = ({questions}) => {
+export const LessonTest: FC<ILessonTest> = ({ questions }) => {
   const navigate = useNavigate();
-  const [questionsState, setQuestionsState] = useState<QuestionDto[]>(questions);
+  const [questionsState, setQuestionsState] =
+    useState<QuestionDto[]>(questions);
 
   useEffect(() => {
-    setQuestionsState(prev => prev.map(question => {
-      return {
-        ...question,
-        answers: question.answers.map(answer => {
-          return {
-            ...answer,
-            isRight: false
-          }
-        })
-      }
-    }))
-  }, [])
+    setQuestionsState(prev =>
+      prev.map(question => {
+        return {
+          ...question,
+          answers: question.answers.map(answer => {
+            return {
+              ...answer,
+              isRight: false,
+            };
+          }),
+        };
+      })
+    );
+  }, []);
 
   const onFinish = () => {
     let good = true;
@@ -36,58 +39,60 @@ export const LessonTest:FC<ILessonTest> = ({questions}) => {
     questions.forEach(question => {
       question.answers.forEach(answer => {
         thuethValues.push(answer.isRight);
-      })
+      });
     });
 
     questionsState.forEach(question => {
       question.answers.forEach(answer => {
         studentValues.push(answer.isRight);
-      })
+      });
     });
 
-    for(let i = 0; i < thuethValues.length; i++) {
-      if(thuethValues[i] !== studentValues[i]) {
-        good = false
-        console.log('')
-      };
+    for (let i = 0; i < thuethValues.length; i++) {
+      if (thuethValues[i] !== studentValues[i]) {
+        good = false;
+        console.log('');
+      }
     }
 
     navigate(-1);
     toast({
       title: good ? 'Тест успешно пройден' : 'Тест пройден с ошибками',
-      variant: good ? 'success' : 'destructive'
+      variant: good ? 'success' : 'destructive',
     });
-  }
+  };
 
-  const onCheckedChange = (entity) => {
-    setQuestionsState(prev => prev.map(question => {
-      if(question.id !== entity.questionId) {
-        return question;
-      } else {
-        return {
-          ...question,
-          answers: question.answers.map(answer => {
-            if(answer.questionId !== entity.answerId) {
-              return answer;
-            } else {
-              return {
-                ...answer,
-                isRight: entity.checked
+  const onCheckedChange = entity => {
+    setQuestionsState(prev =>
+      prev.map(question => {
+        if (question.id !== entity.questionId) {
+          return question;
+        } else {
+          return {
+            ...question,
+            answers: question.answers.map(answer => {
+              if (answer.questionId !== entity.answerId) {
+                return answer;
+              } else {
+                return {
+                  ...answer,
+                  isRight: entity.checked,
+                };
               }
-            }
-          })
+            }),
+          };
         }
-      }
-    }))
-  }
+      })
+    );
+  };
 
-  const contents = questionsState.map((question, index) => <TestItemCard question={question}
-                                                                    onCheckedChange={onCheckedChange}
-  />);
+  const contents = questionsState.map((question, index) => (
+    <TestItemCard question={question} onCheckedChange={onCheckedChange} />
+  ));
   return (
     <LessonContainer>
       <div className="p-8">
-        <FlippingContainer contents={contents} onFinish={onFinish}/>
+        <FlippingContainer onFinish={onFinish} contents={contents} />
       </div>
     </LessonContainer>
   );
--- src/components/shared/flipping-container/flipping-container.tsx
+++ src/components/shared/flipping-container/flipping-container.tsx
@@ -4,33 +4,39 @@ import { Progress } from '@components/ui/progress';
 
 interface IFlippingContainer {
   contents: React.ReactNode[];
-  onFinish: () => void;
+  onFinish?: () => void;
 }
 
-const FlippingContainer:FC<IFlippingContainer> = ({contents, onFinish}) => {
+const FlippingContainer: FC<IFlippingContainer> = ({ contents, onFinish }) => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
   const prev = () => {
-    if(currentIndex > 0) setCurrentIndex(prev => prev-1);
-  }
+    if (currentIndex > 0) setCurrentIndex(prev => prev - 1);
+  };
   const next = () => {
-    if(currentIndex < contents.length-1) {
-      setCurrentIndex(prev => prev+1);
+    if (currentIndex < contents.length - 1) {
+      setCurrentIndex(prev => prev + 1);
     } else {
-      onFinish();
+      onFinish?.();
     }
-  }
+  };
 
   return (
-    <div className='w-full h-full'>
-      <div className='flex items-center justify-between'>
-        <Progress value={((currentIndex+1)*100)/contents.length}/>
-        <div className='w-20 text-right'>{currentIndex+1} из {contents.length}</div>
+    <div className="w-full h-full">
+      <div className="flex items-center justify-between">
+        <Progress value={((currentIndex + 1) * 100) / contents.length} />
+        <div className="w-20 text-right">
+          {currentIndex + 1} из {contents.length}
+        </div>
       </div>
-      <div className='my-6'>{contents[currentIndex]}</div>
-      <div className='w-full flex items-center space-x-3 justify-end'>
-        <Button variant='outline' onClick={prev}>Назад</Button>
-        <Button variant='primary' onClick={next}>{(currentIndex === contents.length-1) ? 'Закончить' : 'Далее'}</Button>
+      <div className="my-6">{contents[currentIndex]}</div>
+      <div className="w-full flex items-center space-x-3 justify-end">
+        <Button variant="outline" onClick={prev}>
+          Назад
+        </Button>
+        <Button variant="primary" onClick={next}>
+          {currentIndex === contents.length - 1 ? 'Закончить' : 'Далее'}
+        </Button>
       </div>
     </div>
   );
